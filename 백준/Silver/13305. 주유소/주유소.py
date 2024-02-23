63d8e4e2d3d8aa1e35f846ce0401a1b2da89a35a
jihyuk0414 <123541776+jihyuk0414@users.noreply.github.com>
2024-02-23 23:58:17 +0900

    [Silver III] Title: 주유소, Time: 112 ms, Memory: 46236 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -5,27 +5,13 @@ N = int(sys.stdin.readline())
 lengthlist = list(map(int,sys.stdin.readline().split()))
 
 moneylist = list(map(int,sys.stdin.readline().split()))
-firstmoney = moneylist.pop(0)
 
-ministmoneystation = moneylist.index(min(moneylist[:len(moneylist)-1]))
+minmoney = moneylist[0]
+totalmoney = 0
 
+for i in range(N-1):
+    if moneylist[i] < minmoney :
+        minmoney = moneylist[i]
+    totalmoney = totalmoney + minmoney * lengthlist[i]
 
-sumlength = sum(lengthlist)
-
-summoney = firstmoney * lengthlist[0]
-#처음 가야할 곳만큼만 충전하기
-sumlength = sumlength - lengthlist[0]
-#처음 간곳 만큼 길이 뺴기
-
-for i in range(len(moneylist)) :
-    if i == ministmoneystation :
-        summoney = summoney + (moneylist[i] * sumlength)
-        break
-        #남은 거리만큼 다 여기서 산다
-
-    else:
-        #최소가 아니라면, 최소만큼만
-        sumoney = summoney + (moneylist[i] * lengthlist[i-1])
-        sumlength = sumlength - lengthlist[i]
-
-print(summoney)
+print(totalmoney)
