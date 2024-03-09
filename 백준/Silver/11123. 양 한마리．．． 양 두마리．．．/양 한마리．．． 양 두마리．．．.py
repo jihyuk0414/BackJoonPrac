dc013ad55dd46a0bd8fb800b7920904b7ecddfd2
jihyuk0414 <123541776+jihyuk0414@users.noreply.github.com>
2024-03-09 22:19:22 +0900

    [Silver II] Title: 양 한마리... 양 두마리..., Time: 196 ms, Memory: 32604 KB -BaekjoonHub

--- 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.py
+++ 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.py
@@ -1,10 +1,12 @@
 import sys
 sys.setrecursionlimit(1000000)
+N = int(sys.stdin.readline())
 
-nx = [-1, 1, 0, 0]
-ny = [0, 0, -1, 1]
+nx = [-1,1,0,0]
+ny = [0,0,-1,1]
 
-def DFS(x, y):
+def DFS(x,y):
+
     global Nlist
     global H
     global W
@@ -12,23 +14,29 @@ def DFS(x, y):
     Nlist[x][y] = '.'
 
     for i in range(4):
-        dx = x + nx[i]
-        dy = y + ny[i]
+        dx = x+nx[i]
+        dy = y+ny[i]
 
-        if (0 <= dx < H) and (0 <= dy < W):
-            if Nlist[dx][dy] == '#':
+        if (0<=dx<H) and (0<=dy<W):
+            if Nlist[dx][dy] == '#' :
+                #아직 안감.
                 Nlist[dx][dy] = '.'
-                DFS(dx, dy)
+                DFS(dx,dy)
+                
 
-N = int(input())
-for _ in range(N):
-    H, W = map(int, input().split())
-    Nlist = [list(input().strip()) for _ in range(H)]  # strip()을 사용하여 개행문자를 제거합니다.
+for i in range(N) :
+    H,W = map(int,sys.stdin.readline().split())
+    Nlist = []
 
-    cnt = 0
+    for j in range(H):
+        Nlist.append(list(sys.stdin.readline().strip()))
+    cnt = 0 
+
     for i in range(H):
         for j in range(W):
-            if Nlist[i][j] == '#':
-                DFS(i, j)
-                cnt += 1
+            if Nlist[i][j] == '#' :
+                DFS(i,j)
+                cnt+= 1
     print(cnt)
+
+    
