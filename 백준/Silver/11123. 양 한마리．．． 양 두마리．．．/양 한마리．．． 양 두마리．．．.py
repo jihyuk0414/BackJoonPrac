32025eda4e111774e1866f555a73ab031c125acb
jihyuk0414 <123541776+jihyuk0414@users.noreply.github.com>
2024-03-09 22:12:19 +0900

    [Silver II] Title: 양 한마리... 양 두마리..., Time: 416 ms, Memory: 32604 KB -BaekjoonHub

--- 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.py
+++ 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.py
@@ -4,7 +4,11 @@ sys.setrecursionlimit(1000000)
 nx = [-1, 1, 0, 0]
 ny = [0, 0, -1, 1]
 
-def DFS(x, y, H, W, Nlist):
+def DFS(x, y):
+    global Nlist
+    global H
+    global W
+
     Nlist[x][y] = '.'
 
     for i in range(4):
@@ -14,18 +18,17 @@ def DFS(x, y, H, W, Nlist):
         if (0 <= dx < H) and (0 <= dy < W):
             if Nlist[dx][dy] == '#':
                 Nlist[dx][dy] = '.'
-                DFS(dx, dy, H, W, Nlist)
+                DFS(dx, dy)
 
 N = int(input())
 for _ in range(N):
     H, W = map(int, input().split())
-    Nlist = [list(input()) for _ in range(H)]
+    Nlist = [list(input().strip()) for _ in range(H)]  # strip()을 사용하여 개행문자를 제거합니다.
 
-    cnt = 0 
+    cnt = 0
     for i in range(H):
         for j in range(W):
             if Nlist[i][j] == '#':
-                DFS(i, j, H, W, Nlist)
+                DFS(i, j)
                 cnt += 1
-
     print(cnt)
