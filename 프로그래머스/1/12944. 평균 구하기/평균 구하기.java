44330c2a2adf7b57a86638121346a97ad838e418
jihyuk0414 <123541776+jihyuk0414@users.noreply.github.com>
2024-04-22 22:36:56 +0900

    [level 1] Title: 평균 구하기, Time: 0.02 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        
+        int total = 0;
+        
+        for (int i =0; i<arr.length; i++)
+        {
+            total+= arr[i];
+        }
+        answer = (double)total / (double) arr.length ;
+        return answer;
+    }
+}
