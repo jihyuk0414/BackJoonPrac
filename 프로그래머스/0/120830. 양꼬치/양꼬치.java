d0a15792d262a73c8efcff8d519adcb27ae1ebb5
jihyuk0414 <123541776+jihyuk0414@users.noreply.github.com>
2024-04-24 11:02:57 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        int servicen = n/10; 
+        
+        answer = (n*12000)+(k*2000) - (servicen*2000);
+
+        return answer;
+    }
+}
