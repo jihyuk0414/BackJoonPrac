d3ed6dfff254deb5f07e8542af74d07f8ea9bd20
jihyuk0414 <123541776+jihyuk0414@users.noreply.github.com>
2024-04-30 20:41:17 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,22 +1,27 @@
 def solution(want, number, discount):
     answer = 0
-    adict = {}
+    answerdict = {}
     
-    for i in range(len(number)) :
-        adict[want[i]] = number[i]
+    for i in range(len(want)) :
+        answerdict[want[i]] = number[i]
     
-    answer = 0
     
-    for i in range(len(discount)-9)  :
-        buydict = adict.copy()
-        for j in range(i,i+10) : 
-            if discount[j] in buydict and buydict[discount[j]] > 0 :
-                buydict[discount[j]] -= 1
-
-                
-        if sum(buydict.values()) == 0 :
-            print(i)
-            answer+=1 
-            
-            
+    for i in range(0,len(discount)-10+1):
+        discountarr = discount[i:i+10] 
+        
+        cntdict = answerdict.copy()
+        
+        for product in discountarr :
+            if product in cntdict.keys():
+                cntdict[product]-=1         
+        
+        zerocnt = True
+        
+        for i in cntdict.values():
+            if i !=0 :
+                zerocnt = False
+                break
+        if zerocnt == True:
+            answer+=1
+        
     return answer
