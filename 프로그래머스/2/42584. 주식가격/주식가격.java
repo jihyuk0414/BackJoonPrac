7c7a437ecace13caaa5ff93e66a46fd31ec5839b
jihyuk0414 <123541776+jihyuk0414@users.noreply.github.com>
2025-02-01 16:11:14 +0900

    [level 2] Title: 주식가격, Time: 36.79 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,15 +1,29 @@
-// 2중 루프 버전
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
-        int len = prices.length;
-        int[] answer = new int[len];
+        int[] answer = new int [prices.length];
+        Stack<Integer> indexstack = new Stack<>();
         
-        for(int i = 0; i < len; i++) {
-            for(int j = i + 1; j < len; j++) {
-                answer[i]++;
-                if(prices[i] > prices[j]) break;
+        for (int i = 0 ; i<prices.length; i++)
+        {
+            int nowval = prices[i];
+            while (!indexstack.isEmpty() && prices[indexstack.peek()] > nowval)
+            {
+                int out = indexstack.pop();
+                answer[out] = i-out;
             }
+            indexstack.push(i);
         }
+        
+        int cnt= 0;
+        
+        while (!indexstack.isEmpty())
+        {
+            int idx = indexstack.pop();
+            answer[idx] = prices.length-1-idx;
+        }
+        
         
         return answer;
     }
