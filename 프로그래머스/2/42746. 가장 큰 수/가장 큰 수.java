ade185152624d4c02e2a1f611c65a619ccf35fc4
jihyuk0414 <123541776+jihyuk0414@users.noreply.github.com>
2025-02-08 15:59:25 +0900

    [level 2] Title: 가장 큰 수, Time: 205.61 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,29 +1,29 @@
-import java.util.* ;
+import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
-        String [] strnum = new String [numbers.length];
-        
-               
-        for(int i = 0 ; i<strnum.length; i++)
-        {
-            strnum[i] = String.valueOf(numbers[i]);
+        // 문자열 배열로 변환
+        String[] strNumbers = new String[numbers.length];
+        for(int i = 0; i < numbers.length; i++) {
+            strNumbers[i] = String.valueOf(numbers[i]);
         }
         
-        Arrays.sort(strnum, (o1,o2) -> 
-                   (o2+o1).compareTo(o1+o2)) ;
+        // Python의 x*3 개념을 동일하게 적용
+        Arrays.sort(strNumbers, (a, b) -> {
+            return (b + b + b).compareTo(a + a + a);
+        });
         
-        if(Integer.parseInt(strnum[0]) ==0 )
-        {
+        // 모든 숫자가 0인 경우 처리
+        if(strNumbers[0].equals("0")) {
             return "0";
         }
         
-        
-        for(int i = 0 ; i<strnum.length; i++)
-        {
-            answer += strnum[i];
+        // 문자열 이어붙이기
+        StringBuilder answer = new StringBuilder();
+        for(String str : strNumbers) {
+            answer.append(str);
         }
-        return answer;
+        
+        return answer.toString();
     }
 }
