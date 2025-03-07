6fd9d1fdba4f9af297feb75f97092e9e6ae3a27e
jihyuk0414 <123541776+jihyuk0414@users.noreply.github.com>
2025-03-07 14:33:01 +0900

    [level 3] Title: 단어 변환, Time: 0.49 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,69 +2,57 @@ import java.util.*;
 
 class Solution {
     public int solution(String begin, String target, String[] words) {
+        int answer = 0;
         
-        Queue<String []> q = new LinkedList<>();
-        
-        //q에 담아서, string,depth
-        String [] first = new String [] {begin,"0"};
-        
+        Queue<String [] > q = new LinkedList<>();
+        String [] newarr = new String[]{begin,"0"};
+        q.add(newarr);
+        //q에다 begin을 넣고
         boolean [] visited = new boolean[words.length];
-        
-        q.add(first);
-        //가능한 배열 모두 찾아서 . empty면 끝
+        //words를 돌면서
         while (!q.isEmpty())
         {
-            String[] now = q.poll();
+            String [] nowarr = q.poll();
+            String now = nowarr[0];
+            int val = Integer.parseInt(nowarr[1]);
             
-            if (now[0].equals(target))
+            if(now.equals(target))
             {
-                return Integer.parseInt(now[1]);
+                return val;
             }
             
-            List<Integer> canbe = canchange(now[0],words);
-            if (canbe.size() != 0)
+            for (int i = 0 ; i<words.length; i++)
             {
-                for (Integer can : canbe)
-                {
-                    if (!visited[can])
-                    {
-                    String [] newstr = new String[2];
-                    newstr[0] = words[can];
-                    newstr[1] = String.valueOf(Integer.parseInt(now[1])+1);
-                    q.add(newstr);
-                    visited[can] = true;
-                        
-                    }
+                String word = words[i];
+                if (!visited[i] && onediff(now,word))
+                { 
+                    visited[i] = true;
+                    String cntstr = String.valueOf(val+1);
+                    q.add(new String [] {word,cntstr});
                 }
             }
-            
         }
+        
         
         return 0;
-
     }
     
-    public List<Integer> canchange (String origin, String [] words)
+    public boolean onediff (String source, String target)
     {
-        //가능한 모든 변환 list
-        List<Integer> canlist = new ArrayList<>();
-        
-        for (int i = 0 ; i<words.length; i++)
+        int cnt = 0;
+        for (int i = 0 ; i<target.length(); i++)
         {
-            String comp = words[i];
-            int check = 0;
-            for (int j = 0 ; j<comp.length(); j++)
+            if(source.charAt(i) != target.charAt(i))
             {
-                if (comp.charAt(j) != origin.charAt(j))
-                {
-                    check+=1;
-                }
-            }
-            if(check == 1)
-            {
-                canlist.add(i);
+                cnt +=1;
             }
         }
-        return canlist;
+        
+        if (cnt == 1)
+        {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
