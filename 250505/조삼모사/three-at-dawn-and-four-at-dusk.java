e9544036dea992dab9eecbb2330bb02dee9c3c79
jihyuk0414 <123541776+jihyuk0414@users.noreply.github.com>
2025-05-05 23:36:00 +0900

    Title: 조삼모사, Time: 648ms, Memory: 22MB, Status: Passed - Codetree

--- 250505/조삼모사/three-at-dawn-and-four-at-dusk.java
+++ 250505/조삼모사/three-at-dawn-and-four-at-dusk.java
@@ -3,9 +3,8 @@ import java.util.*;
 
 public class Main {
 
-    static int N; 
-    static int [] [] Narr;
-    static boolean [] visited ;
+    static int N;
+    static int[][] Narr;
     static int answer = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
@@ -14,34 +13,26 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         N = Integer.parseInt(br.readLine());
-        Narr = new int [N][N];
+        Narr = new int[N][N];
 
-        for (int i = 0 ; i<N; i++)
-        {
-            String [] oneline = br.readLine().split(" ");
-            for (int j = 0 ; j<N; j++)
-            {
+        for (int i = 0; i < N; i++) {
+            String[] oneline = br.readLine().split(" ");
+            for (int j = 0; j < N; j++) {
                 Narr[i][j] = Integer.parseInt(oneline[j]);
             }
         }
 
-        visited = new boolean[N];
+        DFS(new ArrayList<Integer>(), 0);
 
-        DFS(new ArrayList<Integer>(),0);
-
-        bw.write(answer+"");
+        bw.write(answer + "");
         bw.close();
     }
 
-    public static int  calculate(List<Integer> list)
-    {
-        int cnt = 0 ;
-        for (int i = 0 ; i<list.size(); i++)
-        {
-            for (int j = 0 ; j<list.size(); j++)
-            {
-                if (i!= j)
-                {
+    public static int calculate(List<Integer> list) {
+        int cnt = 0;
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.size(); j++) {
+                if (i != j) {
                     int a = list.get(i);
                     int b = list.get(j);
                     cnt += Narr[a][b];
@@ -49,35 +40,25 @@ public class Main {
             }
         }
         return cnt;
-
     }
 
-    public static void  DFS(List<Integer> list , int depth )
-    {
-        if (depth == N/2)
-        {
+    public static void DFS(List<Integer> list, int start) {
+        if (list.size() == N / 2) {
             List<Integer> nochoselist = new ArrayList<>();
-            for (int i = 0 ; i<N; i++)
-            {
-                if (!visited[i])
-                {
+            for (int i = 0; i < N; i++) {
+                if (!list.contains(i)) {
                     nochoselist.add(i);
                 }
             }
-            answer = Math.min(Math.abs(calculate(list) - calculate(nochoselist)),answer);
+
+            answer = Math.min(Math.abs(calculate(list) - calculate(nochoselist)), answer);
             return;
         }
 
-        for (int i = 0 ; i<N; i++)
-        {
-            if (!visited[i])
-            {
-                list.add(i);
-                visited[i] = true;
-                DFS(list,depth+1);
-                visited[i] = false;
-                list.remove(list.size()-1);
-            }
+        for (int i = start; i < N; i++) {
+            list.add(i);
+            DFS(list, i + 1); 
+            list.remove(list.size() - 1); 
         }
     }
 }
