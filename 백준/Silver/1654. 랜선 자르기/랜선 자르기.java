6203712716d0e5a52faa1fb227936f35d37e9d23
jihyuk0414 <123541776+jihyuk0414@users.noreply.github.com>
2025-05-17 18:56:38 +0900

    [Silver II] Title: 랜선 자르기, Time: 172 ms, Memory: 17504 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -4,51 +4,56 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int K,N ; 
+    static int [] arr ; 
+     
+    public static void main(String[] args) throws IOException{
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String [] oneline = br.readLine().split(" ");
-        int K = Integer.parseInt(oneline[0]);
-        int N = Integer.parseInt(oneline[1]);
+        K= Integer.parseInt(oneline[0]);
+        N= Integer.parseInt(oneline[1]);
 
-        int [] Karr = new int [K];
-        
-        for (int i =0 ; i <K  ; i++)
+        arr =new int [K];
+        for (int i = 0; i< K; i++)
             {
-                Karr[i] = Integer.parseInt(br.readLine());
+                arr[i] = Integer.parseInt(br.readLine());
             }
+        int totalsum = 0 ;
+        for (int i = 0 ; i<K ;i++)
+            {
+                totalsum += arr[i];
+            }
+        Arrays.sort(arr);
 
-        Arrays.sort(Karr);
-
-
-        //이때는 index가 아닌, 값임에 주의하자
-        
-        long start = 1 ; 
-        long end = Karr[Karr.length-1];
-
+        long start = 1;
+        long end = arr[K-1];
         long answer = 0 ;
 
         while (start<=end)
             {
-                long mid = (start+end)/2;
+                long mid = (start+(end-start)/2); //가능한 1개의 값
+                int cnt = 0;
 
-                int howmany = 0;
-                for(int i = 0 ; i<K ; i++)
+                for (int i =0 ; i<K; i++)
                     {
-                        howmany += (Karr[i]/mid);
+                        cnt += arr[i]/mid;
                     }
 
-                if(howmany < N)
+                if (cnt >= N)
                 {
-                    end = mid-1;
-                } else {
-                    answer = mid;
+                    //통과 시 
                     start = mid +1;
+                    answer =Math.max(answer,mid);
+                } else {
+                    end = mid-1;
                 }
             }
 
         bw.write(String.valueOf(answer));
         bw.close();
     }
+
 }
