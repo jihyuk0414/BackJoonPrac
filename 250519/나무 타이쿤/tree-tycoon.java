f06c10256071bc8646b3e530e685d7bb43818c15
jihyuk0414 <123541776+jihyuk0414@users.noreply.github.com>
2025-05-19 23:49:49 +0900

    Title: 나무 타이쿤, Time: 115ms, Memory: 10MB, Status: Passed - Codetree

--- 250519/나무 타이쿤/tree-tycoon.java
+++ 250519/나무 타이쿤/tree-tycoon.java
@@ -25,11 +25,13 @@ public class Main {
             }
         }
 
+        // 초기 영양제 위치 설정 - Python 코드와 동일한 순서
         growup.add(new int[]{N-2, 0});
-        growup.add(new int[]{N-2, 1});  
+        growup.add(new int[]{N-2, 1});
         growup.add(new int[]{N-1, 0});
         growup.add(new int[]{N-1, 1});
 
+        // 모든 이동 명령 읽기
         int[][] moves = new int[M][2];
         for (int i = 0; i < M; i++) {
             String[] move = br.readLine().split(" ");
@@ -41,22 +43,27 @@ public class Main {
             int d = moves[day][0];
             int p = moves[day][1];
             
-            Set<String> fertilizerPositions = new HashSet<>();
-
+            // 1. 영양제 이동
             for (int[] grow : growup) {
                 int x = grow[0];
                 int y = grow[1];
 
                 grow[0] = ((x + dx[d] * p) % N + N) % N;
                 grow[1] = ((y + dy[d] * p) % N + N) % N;
-                
+            }
+            
+            // 2. 영양제가 있는 칸의 나무 높이 1 증가
+            for (int[] grow : growup) {
                 map[grow[0]][grow[1]] += 1;
             }
 
+            // 현재 영양제 위치 저장
+            Set<String> fertilizerPositions = new HashSet<>();
             for (int[] grow : growup) {
                 fertilizerPositions.add(grow[0] + "," + grow[1]);
             }
 
+            // 3. 대각선 방향 나무 개수 확인 및 높이 증가
             for (int[] grow : growup) {
                 int x = grow[0];
                 int y = grow[1];
@@ -72,9 +79,10 @@ public class Main {
                         }
                     }
                 }
-                map[x][y] += count;
+                map[x][y] += count; // 대각선에 나무가 있는 개수만큼 증가
             }
 
+            // 4. 새 영양제 구매
             List<int[]> newGrowup = new ArrayList<>();
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
