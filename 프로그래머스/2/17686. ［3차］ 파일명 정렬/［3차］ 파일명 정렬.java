f29162763d0fa845ab27a8ffc84a62d9e6d5d91f
jihyuk0414 <123541776+jihyuk0414@users.noreply.github.com>
2025-06-30 21:34:11 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 30.53 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,81 +1,49 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
-    public List<Character> numberlist = new ArrayList<>();
+    List<String []> answerlist = new ArrayList<>();
     public String[] solution(String[] files) {
-        String [][] comparefiles = new String [files.length][3];
-        
-        for (char i = '0' ; i<='9' ; i++)
+        for (int i = 0 ; i<files.length; i++)
         {
-            numberlist.add(i);
+            String now = files[i];
+            
+            int start = 0;
+            while (start < now.length() && !Character.isDigit(now.charAt(start))) {
+                start++;
+            }
+            
+            String head = now.substring(0, start);
+            
+            int next = start;
+            while (next < now.length() && Character.isDigit(now.charAt(next))) {
+                next++;
+            }
+            
+            String number = now.substring(start, next);
+            String tail = (next == now.length() ? "" : now.substring(next));
+            
+            answerlist.add(new String[]{head, number, tail});
         }
+        
+        Collections.sort(answerlist, 
+                        (a,b) ->
+                         {
+                            if (a[0].toUpperCase().equals(b[0].toUpperCase()))
+                            {
+                                if (Integer.parseInt(a[1]) == Integer.parseInt(b[1]))
+                                {
+                                    return 0;
+                                }
+                                
+                                return Integer.parseInt(a[1])-Integer.parseInt(b[1]);
+                            }
+                            return a[0].toUpperCase().compareTo(b[0].toUpperCase());
+                         });
 
-        for (int i = 0 ; i<files.length;  i++)
-        {
-            String nowstr = files[i];
-            
-            String head = "";
-            String number = "";
-            String tail = "";
-            
-            int nowstatus = 0;
-            int nowindex = 0;
-            while (nowindex < nowstr.length())
-            {
-                if (nowstatus ==0)
-                {
-                    if (numberlist.contains(nowstr.charAt(nowindex)))
-                        {
-                            nowstatus+=1;
-                            continue;
-                        } else {
-                            head += nowstr.charAt(nowindex);
-                            nowindex +=1;  
-                        }
-                } else if (nowstatus ==1)
-                {
-                    if (!numberlist.contains(nowstr.charAt(nowindex)))
-                        {
-                            nowstatus+=1;
-                            continue;
-                        }
-                    number += nowstr.charAt(nowindex);
-                    nowindex +=1;        
-                } else {
-                    tail += nowstr.charAt(nowindex);
-                    nowindex +=1;
-                }
-           }
-            System.out.println(" head : " + head+ " number : "+ number + " tail : " + tail);
-            String [] newarr = new String [] {head,number,tail};
-            comparefiles[i] = newarr;
-        }
-        
-        Arrays.sort(comparefiles, (a,b) -> {
-            if (a[0].toLowerCase().equals(b[0].toLowerCase()))
-            {
-                if (Integer.parseInt(a[1]) == Integer.parseInt(b[1]))
-                {
-                    return 0;
-                } else {
-                    return Integer.parseInt(a[1]) - Integer.parseInt(b[1]);
-                }
-            } else {
-                return a[0].toLowerCase().compareTo(b[0].toLowerCase());
-            }
-        });
-        
-        String [] answer = new String [comparefiles.length];
-        
-        for (int i = 0 ; i<comparefiles.length; i++)
-        {
-            String nowstr2 = "";
-            for (int j = 0 ; j<3 ; j++)
-            {
-                nowstr2+= comparefiles[i][j];
-            }
-            answer [i] = nowstr2;
+        String[] answer = new String[files.length];
+        for (int i = 0; i < answerlist.size(); i++) {
+            String[] parts = answerlist.get(i);
+            answer[i] = parts[0] + parts[1] + parts[2];
         }
         
         return answer;
