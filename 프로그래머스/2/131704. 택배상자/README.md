db043ebdaee8dc2eef849f22e91273e419733435
jihyuk0414 <123541776+jihyuk0414@users.noreply.github.com>
2025-07-15 21:31:11 +0900

    [level 2] Title: 택배상자, Time: 183.74 ms, Memory: 183 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/README.md
+++ 프로그래머스/2/131704. 택배상자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 택배상자 - 131704 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131704) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131704#) 
 
 ### 성능 요약
 
-메모리: 178 MB, 시간: 191.34 ms
+메모리: 183 MB, 시간: 183.74 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 02일 16:10:43
+2025년 07월 15일 21:31:07
 
 ### 문제 설명
 
--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -3,52 +3,67 @@ import java.util.*;
 class Solution {
     public int solution(int[] order) {
         int answer = 0;
-        int N = order.length; 
-        
-        Queue<Integer> rail = new LinkedList<>();
-        Stack<Integer> bozo = new Stack<>();
-        
-        for (int i =1 ; i<=N;  i++)
-        {
-            rail.add(i);
-        }
+           
+        //order랑 똑같은거 만들어서 넣기
+        int [] neworder = new int [order.length];
         
         for (int i = 0 ; i<order.length; i++)
         {
-            int now =order[i];
-            
-            if (!bozo.isEmpty())
-            {
-                if (bozo.peek() == now)
-                {
-                    bozo.pop();
-                    answer+=1;
-                    continue;
-                }
-            }
-            
-            while (!rail.isEmpty() && rail.peek() != now )
-            {
-                //레일엔 뭐가 있고 지금 당장 기사줄수 없을 때 
-                bozo.push(rail.poll());
-            }
-            
-            if (!rail.isEmpty())
-            {
-                if (rail.peek() == now)
-                {
-                    rail.poll();
-                    answer+=1;
-                    continue;
-                }
-            } 
-            
-            if (rail.isEmpty())
-            {
-                break;
-            }
+            neworder[order[i]-1] = i;
         }
         
-        return answer;
-    }
+        Queue<Integer> container = new LinkedList<>();
+        for (int i = 0 ; i<neworder.length; i++)
+        {
+            container.add(neworder[i]); //삐뽀
+        }
+        
+        Stack<Integer> subcontainer = new Stack<>();
+        
+        int target= 0;
+        while(!container.isEmpty())
+            {
+                int containerval = container.peek();
+                if(containerval == target)
+                {
+                    target+=1;
+                    answer+=1;
+                    container.poll();
+                }
+                else
+                {
+                    //컨테이너 젤 앞께 다르다. 
+                    if(!subcontainer.isEmpty() && subcontainer.peek() == target)
+                    {
+                        target+=1;
+                        answer+=1;
+                        subcontainer.pop();
+                        continue;
+                    }
+                    else
+                    {
+                        //없으면 일단 넣어
+                        subcontainer.push(container.poll());
+                    }
+                }
+            }
+            
+            while(!subcontainer.isEmpty())
+            {
+                if(subcontainer.peek() != target)
+                {
+                    return answer;
+                }
+                else
+                {
+                    target+=1;
+                    answer+=1;
+                    subcontainer.pop();
+                }
+            }
+            
+            return answer;
+            
+        }
+        
 }
