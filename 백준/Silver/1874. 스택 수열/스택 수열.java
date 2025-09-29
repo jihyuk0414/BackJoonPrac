2d3823a6bf680c1e1096c1a93bde1444405b6549
jihyuk0414 <123541776+jihyuk0414@users.noreply.github.com>
2025-09-29 22:19:38 +0900

    [Silver II] Title: 스택 수열, Time: 296 ms, Memory: 27624 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,67 +1,47 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
-class Main {
+public class Main {
     public static void main(String[] args) throws IOException {
-        // BufferedReader와 BufferedWriter 객체 생성
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
-        // N을 입력 받음
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
-        // 수열을 입력 받음
-        List<Integer> answerList = new ArrayList<>();
+        
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        
+        int start = 1; // push할 숫자
+        boolean possible = true;
+        
         for (int i = 0; i < N; i++) {
-            answerList.add(Integer.parseInt(br.readLine()));
-        }
-
-        int nownum = 1;
-        int compareIndex = 0;
-
-        Stack<Integer> myStack = new Stack<>();
-        List<Character> correctList = new ArrayList<>();
-
-        boolean isPossible = true;
-
-        while (true) {
-            if (compareIndex == N) {
-                break;
-            }
-
-            if (nownum > N + 1) {
-                isPossible = false;
-                break;
-            }
-
-            if (myStack.isEmpty()) {
-                myStack.push(nownum);
-                nownum += 1;
-                correctList.add('+');
-            } else if (answerList.get(compareIndex).equals(myStack.peek())) {
-                myStack.pop();
-                compareIndex += 1;
-                correctList.add('-');
+            int value = Integer.parseInt(br.readLine());
+            
+            if (value >= start) {
+                // start부터 value까지 push
+                while (start <= value) {
+                    stack.push(start++);
+                    sb.append("+\n");
+                }
+                // value를 pop
+                stack.pop();
+                sb.append("-\n");
             } else {
-                myStack.push(nownum);
-                nownum += 1;
-                correctList.add('+');
+                // 이미 스택에 value가 있어야 하는데 그렇지 않으면 NO
+                if (stack.isEmpty() || stack.peek() != value) {
+                    possible = false;
+                    break;
+                }
+                stack.pop();
+                sb.append("-\n");
             }
         }
-
-        if (isPossible) {
-            StringBuilder sb = new StringBuilder();
-            for (char op : correctList) {
-                sb.append(op);
-                sb.append('\n'); // 각 연산 사이에 줄바꿈 추가
-            }
-            bw.write(sb.toString().trim()); // 최종 결과를 줄바꿈 없이 출력
+        
+        if (possible) {
+            System.out.print(sb.toString());
         } else {
-            bw.write("NO");
+            System.out.println("NO");
         }
-
-        // BufferedWriter를 사용하여 결과를 출력하고 스트림을 닫음
-        bw.flush();
-        bw.close();
     }
 }
