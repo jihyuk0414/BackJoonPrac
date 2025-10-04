c92ba86aadb78770cb07e6847ffab80472ffbeb4
jihyuk0414 <123541776+jihyuk0414@users.noreply.github.com>
2025-10-04 19:52:12 +0900

    [Silver I] Title: 회전 초밥, Time: 236 ms, Memory: 22268 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -4,58 +4,57 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String [] inputarr = br.readLine().split(" ");
-        
-        int N = Integer.parseInt(inputarr[0]);
-        int d = Integer.parseInt(inputarr[1]);
-        int k = Integer.parseInt(inputarr[2]);
-        int c = Integer.parseInt(inputarr[3]);
-
-        int [] originalarr = new int[N*2];
-        
-        for (int i = 0 ; i<N ; i++)
+        String [] oneline = br.readLine().split(" ");
+        int N = Integer.parseInt(oneline[0]);
+        int d = Integer.parseInt(oneline[1]);
+        int k = Integer.parseInt(oneline[2]);
+        int c = Integer.parseInt(oneline[3]);
+        int answer= 0 ;
+        int []  Nlist = new int [N*2];
+        for (int i = 0 ; i<N; i++)
             {
-                int oneinput = Integer.parseInt(br.readLine());
-                originalarr[i] = oneinput;
-                originalarr[i+N] = oneinput;
+                int go = Integer.parseInt(br.readLine());
+                Nlist[i] = go;
+                Nlist[N+i] = go;
             }
 
-        //계속 비교해갈 Queue
-        Queue<Integer> originalq = new LinkedList<>();
-        
-        //최초k개
-        for (int i = 0 ; i < k ; i++)
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for (int i = 0 ; i<k; i++)
             {
-                originalq.add(originalarr[i]);
+                map.put(Nlist[i],map.getOrDefault(Nlist[i],0)+1);
             }
+       if (!map.containsKey(c)) {
+        answer = map.size() + 1; 
+        } 
+       else {
+            answer = map.size();
+        }
 
-        int start = 0 ; 
-        int end = k-1; 
-        int answer = 0 ;
-    
-        while (start<N)
+        for (int i = k ; i<N+k ; i++)
             {
-                Set<Integer> myset = new HashSet<>(originalq);
+                map.put(Nlist[i],map.getOrDefault(Nlist[i],0)+1);
+                map.put(Nlist[i-k],map.get(Nlist[i-k])-1);
+                if(map.get(Nlist[i-k]) == 0)
+                {
+                    map.remove(Nlist[i-k]);
+                }
 
-                myset.add(c);
-
-                answer = Math.max(answer,myset.size());
-
-                start +=1 ;
-                end+=1;
-
-                originalq.poll();
-                originalq.add(originalarr[end]);
-                
+                int cnt = map.keySet().size();
+                if(!map.keySet().contains(c))
+                {
+                    cnt +=1;
+                }
+                 answer = Math.max(answer,cnt);
             }
 
         bw.write(String.valueOf(answer));
         bw.close();
+        
         
     }
 }
