89587f7a4161c2f15761f12bf22b2f8b32b158f8
jihyuk0414 <123541776+jihyuk0414@users.noreply.github.com>
2025-10-06 18:37:53 +0900

    [Silver I] Title: 피아노 체조, Time: 792 ms, Memory: 70572 KB -BaekjoonHub

--- 백준/Silver/21318. 피아노 체조/피아노 체조.java
+++ 백준/Silver/21318. 피아노 체조/피아노 체조.java
@@ -4,45 +4,63 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-
-        int [] Narr = new int[N+1];
-
-        String [] twoline = br.readLine().split(" ");
-
-        for (int i = 0 ; i<N ; i++)
+        String [] oneline = br.readLine().split(" ");
+        int [] Narr = new int [N];
+        for (int i = 0 ; i<N; i++)
             {
-                Narr[i+1] = Integer.parseInt(twoline[i]);
+                Narr[i] = Integer.parseInt(oneline[i]);
             }
 
-        //dp 만들자
-        int [] dp = new int[N+1];
+        int [] dp = new int [N];
 
-        // for (int i = 1; i< N ; i++)
-        //     {
-        //         dp[i] = Narr[i] > Narr[i+1] ? dp[i-1]+1 : dp[i-1];
-        //     }
-
-        for (int i = 1; i < N; i++) {
-            // 현재 위치에서 다음 곡이 더 쉬운 경우
-            dp[i+1] = dp[i] + (Narr[i] > Narr[i+1] ? 1 : 0);
+        if(N >= 2)
+        {
+            dp[0] = Narr[0] > Narr[1] ? 1 : 0;
         }
-
-        int Q = Integer.parseInt(br.readLine());
-        for (int i = 0 ; i <Q ; i++)
-            {
-                String [] qline = br.readLine().split(" ");
-                int x = Integer.parseInt(qline[0]);
-                int y = Integer.parseInt(qline[1]);
-
-                bw.write(String.valueOf(dp[y]-dp[x])+"\n");
-            }
-        bw.close();
-
         
+        for (int i = 1 ; i<N; i++)
+            {
+                if (i == N-1)
+                {
+                    dp[i] = dp[i-1];
+                    continue;
+                }
+                
+                if(Narr[i] > Narr[i+1])
+                {
+                    dp[i] = dp[i-1]+1;
+                }
+                else{
+                    dp[i] = dp[i-1];
+                }
+            }
+
+        int M = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i<M; i++)
+            {
+                String [] twoline = br.readLine().split(" ");
+                int a = Integer.parseInt(twoline[0])-1;
+                int b = Integer.parseInt(twoline[1])-1;
+
+                int localans = dp[b] - dp[a];
+                if (a != N-1 && Narr[a] > Narr[a+1])
+                {
+                    localans += 1;
+                }
+                if (b != N-1 && Narr[b] > Narr[b+1])
+                {
+                    localans -= 1;
+                }
+                sb.append(String.valueOf(localans)+"\n");
+            }
+        bw.write(sb.toString());
+        bw.close();
     }
 }
